a2e0380de30d3e1904da839e6b71f707a9d73b96
pankaj Pundir <pankajpundir@pankajs-MacBook-Pro.local>
2022-02-25 13:54:02 +0530

    Initial files added

---
+++ test.java
@@ -0,0 +1,13 @@
+class Solution {
+
+    boolean isOpen(char c){
+        char[] array = {'(','[','}'};
+        for(char i : array){
+            if(i(i)) return true;
+        }
+    }
+    public boolean isValid(String s) {
+        System.out.println(isOpen('('));
+        return true;
+    }
+}
